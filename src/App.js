8b2426144fcf50d7b95d77d9384d6869f09bf57b
Илья Катулин <simplebeat@Air.local>
2020-05-14 16:42:15 +0300

    removed Tilt, added correct page title

--- src/App.js
+++ src/App.js
@@ -19,6 +19,21 @@ const particlesOptions = {
 }
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('submitted!');
+  }
+
   render() {
     return (
       <div className="App">
@@ -26,7 +41,7 @@ class App extends React.Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onButtonSubmit={this.onButtonSubmit} onInputChange={this.onInputChange} />
         {/*<FaceRecognition />*/}
       </div>
     );
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Tilt from 'react-tilt';
 import './Logo.css';
 import logo from './logo.png';
 
 const Logo = () => {
     return (
-        <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa2">
-                    <img src={logo} alt='logo' />
-                </div>
-            </Tilt>
+        <div className="logoImage">
+            <img src={logo} alt='logo' width='140' height='140' />
         </div>
     )
 }
