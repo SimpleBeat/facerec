6f1214bbd23653bc705530c77b8d562e6d772082
SimpleBeat <simplebeat@simplebeat.com>
2020-05-27 19:16:17 +0300

    Minor fixes for deployment

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends React.Component {
   onButtonSubmit = () => {
     this.setState({ imageURL: this.state.input });
 
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://morning-chamber-08121.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ 
@@ -86,7 +86,7 @@ class App extends React.Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://morning-chamber-08121.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({ 
